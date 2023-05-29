c2d6fa7881753f2b280df4520f9bed7f8b8574ed
박재두 <@DESKTOP-338PH6J>
2023-05-29 16:54:08 +0900

    week12

---
+++ solveRangelnPerifocalFrame.m
@@ -0,0 +1,12 @@
+function rangelnPQW = solveRangelnPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
+
+% input: semimajor_axis,eccentricity,true_anomaly(deg)
+% output: rangeInPQW(3X1행렬)
+
+mu = 398600; %(km^3/m^-2)
+
+r = semimajor_axis*(1-eccentricity^2)/(1+eccentricity*cos(true_anomaly));%lesson04 12p: r = a*(1-e^2)/(1+e*cos nu) 
+
+rangelnPQW = [r*cos(true_anomaly); r*sin(true_anomaly); 0];%lesson07 12p
+
+end
---
+++ solveVelocitylnPerifocalFrame.m
@@ -0,0 +1,11 @@
+function velocityInPQW = solveVelocitylnPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
+
+% input: semimajor_axis,eccentricity,true_anomaly(deg)
+% output: rangeInPQW(3X1행렬)
+
+mu = 398600; %(km^3/m^-2)
+p = semimajor_axis*(1 - eccentricity^2) %lesson04 18p
+
+velocityInPQW = sqrt(mu/p)*[-sin(ture_anomaly); eccentricity + cos(true_anomaly); 0];%lesson07 14p: v_pq = sqrt(mu/a)*[-sin(nu); eccentricity+cos(nu); 0] 
+
+end
