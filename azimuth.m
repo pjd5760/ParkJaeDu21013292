c9c8561b2d9c12d12efad35b15317bde9bc1887a
박재두 <@DESKTOP-338PH6J>
2023-06-04 22:57:42 +0900

    week13

--- azimuth.m
+++ azimuth.m
@@ -1,8 +1,8 @@
 function az = azimuth(ENU)
-    % ENU 좌표를 라디안으로 변환합니다.
+    % ENU 좌표를 라디안으로 변환
     ENU_rad = deg2rad(ENU);
     
-    % E, N, U 좌표를 추출합니다.
+    % E, N, U
     E = ENU_rad(:, 1);
     N = ENU_rad(:, 2);
     U = ENU_rad(:, 3);
@@ -11,9 +11,9 @@ function az = azimuth(ENU)
     az_rad = atan2(E, N);
     az_deg = rad2deg(az_rad);
     
-    % 방위각을 [0, 360) 범위로 조정합니다.
+    % 방위각을 [0, 360) 범위로 조정
     az_deg = mod(az_deg + 360, 360);
     
-    % 방위각을 출력합니다.
+    % 방위각
     az = az_deg;
 end
